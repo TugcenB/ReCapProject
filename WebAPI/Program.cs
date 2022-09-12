307cba8d1ce4555d2856d40cea6370245b51f428
BilginTugcenur <92152213+BilginTugcenur@users.noreply.github.com>
2022-09-12 21:21:44 +0300

    Autofac integration

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -11,23 +14,28 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
-builder.Services.AddSingleton<IBrandService,BrandManager>();
+//builder.Services.AddSingleton<IBrandDal, EfBrandDal>();
+//builder.Services.AddSingleton<IBrandService,BrandManager>();
 
-builder.Services.AddSingleton<IColorDal, EfColorDal>();
-builder.Services.AddSingleton<IColorService,ColorManager>();
+//builder.Services.AddSingleton<IColorDal, EfColorDal>();
+//builder.Services.AddSingleton<IColorService,ColorManager>();
 
-builder.Services.AddSingleton<IUserDal,EfUserDal>();
-builder.Services.AddSingleton<IUserService,UserManager>();
+//builder.Services.AddSingleton<IUserDal,EfUserDal>();
+//builder.Services.AddSingleton<IUserService,UserManager>();
 
-builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
-builder.Services.AddSingleton<ICustomerService,CustomerManager>();
+//builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
+//builder.Services.AddSingleton<ICustomerService,CustomerManager>();
 
-builder.Services.AddSingleton<ICarDal,EfCarDal>();
-builder.Services.AddSingleton<ICarService,CarManager>();
+//builder.Services.AddSingleton<ICarDal,EfCarDal>();
+//builder.Services.AddSingleton<ICarService,CarManager>();
 
-builder.Services.AddSingleton<IRentalDal,EfRentalDal>();
-builder.Services.AddSingleton<IRentalService,RentalManager>();
+//builder.Services.AddSingleton<IRentalDal,EfRentalDal>();
+//builder.Services.AddSingleton<IRentalService,RentalManager>();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(builder => builder.RegisterModule(new AutofacBusinessModule()));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
